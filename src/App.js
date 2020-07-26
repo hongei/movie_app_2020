6db5956b88636b2feabb6bdd56315b1f620bb9e0
wodml12 <hong699812@gmail.com>
2020-07-26 20:45:12 +0900

    영화 앱을 만들기 전 간단한 음식 앱 구현

--- src/App.js
+++ src/App.js
@@ -1,26 +1,59 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+function Food({ name, picture }){ 
+  return (
+    <div>
+      <h2>I Like {name}</h2>
+      <img src={picture} alt='foodImg'></img>
+    </div>
+  );
+}
+
+const foodILike = [
+  {
+    id: '1',
+    name: 'Kimci',
+    image: 'https://m.jnmall.kr/web/product/big/201910/4b83072de272a51edffa420ab3b2fa98.jpg'
+  },
+  {
+    id: '2',
+    name: 'Bulgogi',
+    image: 'https://recipe1.ezmember.co.kr/cache/recipe/2019/03/03/11baafbe81803965b17c3ab42a5992cb1.jpg'
+  },
+  {
+    id: '3',
+    name: 'Bibimbap',
+    image: 'https://recipe1.ezmember.co.kr/cache/recipe/2018/10/03/355b5cd5c3beb1a775c82ee425dcd1931.jpg'
+  },
+  {
+    id: '4',
+    name: 'Doncasu',
+    image: 'https://th2.tmon.kr/thumbs/image/423/336/e17/1b1492318_700x700_95_FIT.jpg'
+  },
+  {
+    id: '5',
+    name: 'Kimbap',
+    image: 'https://recipe1.ezmember.co.kr/cache/recipe/2016/06/29/e7401296033ab8e4297cd53d71e1bba91.jpg'
+  }
+];
+
+// '=>' 으로 함수 작성 하는 형태
+// const renderFood = (dish) => <Food name={dish.name} picture={dish.image} key={dish.id} />
+
+//기존에 function 함수명(){}으로 작성 하는 형태
+// function renderFood(dish){
+//   return <Food name={dish.name} picture={dish.image} key={dish.id} />
+// }
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {/* {foodILike.map(renderFood)} */}
+      {foodILike.map((dish) => (
+        <Food name={dish.name} picture={dish.image} key={dish.id} />
+      ))}
     </div>
-  );
+    );
 }
 
 export default App;
